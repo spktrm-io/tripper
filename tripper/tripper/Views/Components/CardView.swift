f3bf63706aad168b1f30a5a5700626a6a7f8ea30
brcls <petrick_barcelos@hotmail.com>
2024-09-15 21:39:00 -0300

    ajustados erros no tabMenu e thema

---
+++ tripper/tripper/Views/Components/CardView.swift
@@ -0,0 +1,8 @@
+//
+//  CardView.swift
+//  tripper
+//
+//  Created by Erick Barcelos on 15/09/24.
+//
+
+import Foundation
---
+++ tripper/tripper/Views/Components/ToggleButtonsView.swift
@@ -0,0 +1,8 @@
+//
+//  ToggleButtonsView.swift
+//  tripper
+//
+//  Created by Erick Barcelos on 15/09/24.
+//
+
+import Foundation
